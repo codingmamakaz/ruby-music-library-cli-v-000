1ff06c173de0ab119c4ea0262de94e74e03da922
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-03 04:22:39 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,6 +11,8 @@ class MusicLibraryController
   end
 
   def call
+    input = ""
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -20,13 +22,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets
-    "exit"
-    3.times do
-      input = gets
+    input = gets.strip
     
-
-    end
 
   end
 
