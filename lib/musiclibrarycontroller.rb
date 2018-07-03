31b3ae300ba3e20a8bf16ff81a262b37b4e17499
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-03 03:51:56 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -13,6 +13,7 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
+    gets list_songs
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
@@ -24,7 +25,7 @@ class MusicLibraryController
     "exit"
     3.times do
       gets
-      
+
     end
   end
 
