cd3dd25a40a1bf7153a57ea7e032fb0d81ebb252
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-03 04:03:17 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -13,25 +13,19 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
-    list_songs if gets == "list songs"
     puts "To list all of the artists in your library, enter 'list artists'."
-    list_artists if gets == "list artists"
     puts "To list all of the genres in your library, enter 'list genres'."
-    list_genres if gets
     puts "To list all of the songs by a particular artist, enter 'list artist'."
-    list_songs_by_artist if gets
     puts "To list all of the songs of a particular genre, enter 'list genre'."
-    list_songs_by_genre if gets
     puts "To play a song, enter 'play song'."
-    play_song if gets
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     gets
-    # "exit"
-    # 3.times do
-    #   gets
+    "exit"
+    3.times do
+      gets
 
-    # end
+    end
   end
 
   def list_songs
